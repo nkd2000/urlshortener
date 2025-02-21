5322af9eb647ff2a77fc533bf119aa5d04e2dedd
Nilesh Kumar Dahariya <nileshdaha2000@gmail.com>
2025-02-21 10:55:28 +0000

    ready for submission

--- app.js
+++ app.js
@@ -15,32 +15,33 @@ app.use('/public',express.static(`${process.cwd()}/public`));
 
 let user_url = '/';
 let valid_url = false;
-
+let urlId = 0;
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/view/index.html');
 });
 
 app.post('/api/shorturl',(req,res)=>{
     user_url = req.body.userUrl;
-    if(!user_url) return res.status(400).json({error:'invalid url'});
+    if(!user_url) return res.json({error:'invalid url'});
 
     try {
         const hostname = new URL(user_url).hostname;
         dns.lookup(hostname, (err)=>{
-            if(err) return res.status(400).json({error:'invalid url'});
-            res.json({original_url:user_url,short_url:1});
+            if(err) return res.json({error:'invalid url'});
+            urlId = Math.round(Math.random()*1000);
+            res.json({original_url:user_url,short_url:urlId});
             valid_url = true;
         });
     } catch (error) {
-        return res.status(400).json({error:'invalid url'});
+        return res.json({error:'invalid url'});
     }
 
    
 });
 
-app.get('/api/shorturl/1',(req,res)=>{
-    if(valid_url) res.redirect(user_url);
-    else res.redirect('/');
+app.get('/api/shorturl/:urlId',(req,res)=>{
+    if(valid_url) return res.redirect(user_url);
+    else return res.json({error:'invalid url'});
 });
 
 const listner = app.listen(process.env.PORT || 5000, ()=>{console.log(`App is listening on port ${listner.address().port}`)});
